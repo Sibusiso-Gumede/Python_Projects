53638376be19e12d6f02049eaa2fb28f3165ca9f
Sibusiso J. Gumede <96148580+Sibusiso-Gumede@users.noreply.github.com>
2022-02-20 09:20:56 +0200

    Delete pizzeria directory

--- pizzeria/pizzeria_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register the models in the admin site.
-from .models import Pizza
-from .models import Topping
-
-admin.site.register(Pizza)
-admin.site.register(Topping)
--- pizzeria/pizzeria_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PizzeriaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pizzeria_app'
